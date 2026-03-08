ae2b4023883f9660209fa09bb4f0ac46c2916ef0
Elias <3elias3rd@gmail.com>
2026-03-08 15:41:30 +0400

    Add categories seed script

---
+++ categories_to_db.py
@@ -0,0 +1,37 @@
+from models import Category
+from models import SessionLocal
+
+db = SessionLocal()
+
+def seed_categories():
+    categories = [
+        Category(
+        name="client_entertainment",
+        deductible_pct=0.5
+        ),
+        Category(
+        name="rent_and_utilities",
+        deductible_pct=1.0
+        ),
+        Category(
+        name="marketing_and_ads",
+        deductible_pct=1.0
+        ),
+        Category(
+        name="salaries_and_visas",
+        deductible_pct=1.0
+        ),
+        Category(
+        name="fines_and_penalties",
+        deductible_pct=0
+        )
+    ]
+
+    db.add_all(categories)
+    db.commit()
+    db.close()
+
+    return {"status": "Categories successfuly seeded"}
+
+if db.query(Category).count() == 0:
+    seed_categories()
